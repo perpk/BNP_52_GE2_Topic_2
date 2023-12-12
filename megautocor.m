1938e3988c83040999f56e707d5306bc1c2c9a34
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2023-12-12 13:16:55 +0200

    initial commit w. given files

---
+++ megautocor.m
@@ -0,0 +1,65 @@
+%    EPILEPTIC MEG AUTOCORRELATION
+clear all;
+close all;
+clc;
+display('EPILEPTIC MEG autocorrelation estimation');
+
+pkg load signal;
+
+% load MEG data
+a = load('rfot27.flt');
+
+% sampling frequency
+fs = 256;
+
+% signal length and starting sample
+N = 1024;
+shift = 1000;
+
+% signal
+y = a(shift+1:shift+N);
+
+% remove mean value
+y = y - mean(y);
+
+% time values
+t = (0:N-1)/fs;
+
+ %    PLOT SIGNAL
+figure(1);
+plot(t,y);
+axis([0 (N-1)/fs min(y) max(y)]);
+xlabel('TIME (sec)');
+ylabel('MEG (pT)');
+title('EPILEPTIC MEG SIGNAL');
+
+%  ESTIMATE AND PLOT AUTO_CORRELATION
+
+figure(2);
+cr = xcorr(y, y, 'coeff');
+plot((-(N-1):(N-1))/fs, cr, 'r');
+axis([-(N-1)/fs (N-1)/fs min(cr) max(cr)]);
+xlabel('TIME(sec)');
+ylabel('AUTOCORRELATION COEFICIENT');
+title('EPILEPTIC MEG AUTOCORRELATION ESTIMATION');
+
+
+% track first zero-crossing
+i = N;
+do
+  i++;
+until (cr(i) < 0)
+fprintf('first zero of autocorrelation coefficient at %d\n', i- N-1);
+
+k=[1, i-N-1];
+% LAG PLOT 
+figure(3);
+
+for j=1:numel(k)
+  subplot(1,numel(k),j)
+  scatter(y(2+k(j):end),y(1:end-1-k(j)))
+  xlabel(['y(i-',num2str(k(j)),')']);
+  ylabel('y(i)');
+  title(['EPILEPTIC MEG PHASE PLOT FOR tau=',num2str(k(j))]);
+endfor
+
---
+++ megphase.m
@@ -0,0 +1,54 @@
+% EPILEPTIC MEG phase space trajectory 
+clear all;
+close all;
+clc;
+display('EPILEPTIC MEG phase space reconstruction');
+
+% load MEG data
+a = load('rfot27.flt');
+
+% signal length, starting sample and reconstruction parameter tau
+N = 1024;
+shift = 1000;
+tau = 1;
+
+% phase space reconstruction vectors
+x2 = zeros(N-tau,2);
+x3 = zeros(N-2*tau,3);
+fs = 256;
+
+for i = 1:N-tau
+   x2(i, 1:2) = [a(shift+i), a(shift+i+tau)];
+end
+
+
+for i = 1:N-2*tau
+   x3(i, 1:3) = [a(shift+i), a(shift+i+tau), a(shift+i+2*tau)];
+end
+
+% plot signal
+figure(1);
+plot((0:N-1)/fs, a(shift+1:shift+N));
+axis([0 N/fs min(a(shift+1:shift+N)) max(a(shift+1:shift+N))]);
+xlabel('TIME (sec)');
+ylabel('MEG AMPLITUDE (pT)');
+title('EPILEPTIC MEG SIGNAL');
+
+% 2-D phase space reconstruction
+
+figure(2);
+plot(x2(:,1), x2(:,2));
+xlabel('X');
+ylabel('Y');
+title('EPILEPTIC MEG 2-D PHASE SPACE RECONSTRUCTION');
+
+% 3-D phase space reconstruction
+
+figure(3);
+plot3(x3(:,1), x3(:,2), x3(:,3));
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('EPILEPTIC MEG 3-D PHASE SPACE RECONSTRUCTION');
+view(45,10);
+
